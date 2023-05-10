9c05273bd037131294a32a1c05f1e48b754622e5
zpolonius <zacharias@polonius.dk>
2023-05-10 21:31:46 +0200

    added function in setState and textfield to show the changes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
   createState() {
     return AppState();
   }
 }
 
 class AppState extends State<App> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,10 +20,15 @@ class AppState extends State<App> {
           'Let\'s see some images',
         ),
       ),
+      body: Center(
+        child: Text('$counter images'),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          print('Button has been pressed');
+          setState(() {
+            counter += 1;
+          });
         },
       ),
     ));
